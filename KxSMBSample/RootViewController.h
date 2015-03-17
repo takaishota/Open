78f5038a7aa7feb92274bc67a54c52918472616b
Shota Takai <s-takai@Shota-no-MacBook-Pro.local>
2015-03-17 16:43:26 +0900

    新規クラスを追加

---
+++ KxSMBSample/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  SMBFileReader
+//
+//  Created by Shota Takai on 2015/03/17.
+//  Copyright (c) 2015年 Konstantin Bukreev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RootViewController : NSObject
+
+@end
---
+++ KxSMBSample/RootViewController.m
@@ -0,0 +1,13 @@
+//
+//  RootViewController.m
+//  SMBFileReader
+//
+//  Created by Shota Takai on 2015/03/17.
+//  Copyright (c) 2015年 Konstantin Bukreev. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@implementation RootViewController
+
+@end

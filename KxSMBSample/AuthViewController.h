927d58d1ae604c3500196e6ac1e5addaf6335e06
Shota Takai <s-takai@Shota-no-MacBook-Pro.local>
2015-03-19 11:27:37 +0900

    認証情報の入力画面のためのクラスを追加

---
+++ KxSMBSample/AuthViewController.h
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.h
+//  SMBFileReader
+//
+//  Created by Shota Takai on 2015/03/19.
+//  Copyright (c) 2015年 Konstantin Bukreev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AuthViewController : UIViewController
+
+@end
---
+++ KxSMBSample/AuthViewController.m
@@ -0,0 +1,37 @@
+//
+//  AuthViewController.m
+//  SMBFileReader
+//
+//  Created by Shota Takai on 2015/03/19.
+//  Copyright (c) 2015年 Konstantin Bukreev. All rights reserved.
+//
+
+#import "AuthViewController.h"
+
+@interface AuthViewController ()
+
+@end
+
+@implementation AuthViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

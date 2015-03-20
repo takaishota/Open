d27411a40ff85854d351c2f17d050ae6692704d4
Shota Takai <s-takai@Shota-no-MacBook-Pro.local>
2015-03-20 12:21:28 +0900

    ホーム画面のビューコントローラークラスを新規作成

---
+++ KxSMBSample/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  SMBFileReader
+//
+//  Created by Shota Takai on 2015/03/20.
+//  Copyright (c) 2015年 Konstantin Bukreev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end

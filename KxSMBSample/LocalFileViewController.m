dcf2a7992792eefdde4516e74fac68f24eb15a2c
Shota Takai <s-takai@Shota-no-MacBook-Pro.local>
2015-03-26 12:45:35 +0900

    ローカルファイルの削除機能追加

--- KxSMBSample/LocalFileViewController.m
+++ KxSMBSample/LocalFileViewController.m
@@ -7,21 +7,23 @@
 //
 
 #import "LocalFileViewController.h"
+#import "FileUtility.h"
 
-@interface LocalFileViewController ()
+@interface LocalFileViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @end
 
-@implementation LocalFileViewController
+@implementation LocalFileViewController {
+    NSArray *_items;
+    NSMutableArray* _datasource;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    self.title = @"ローカル";
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    _items = [self getLocalFiles];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -29,72 +31,74 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSArray *)getLocalFiles {
+    
+    NSString *docPath = [[FileUtility sharedUtility] documentDirectory];
+    NSArray *fileNames = [[FileUtility sharedUtility] fileNamesAtDirectoryPath:docPath extension:nil];
+    _datasource = [fileNames mutableCopy];
+    
+    return _datasource;
+}
+
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return [_items count];
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1
+                                      reuseIdentifier:cellIdentifier];
+    }
     
-    // Configure the cell...
+    NSString *item = _items[indexPath.row];
+    cell.textLabel.text = item;
+    
+//    if ([item isKindOfClass:[KxSMBItemTree class]]) {
+//        
+//        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+//        cell.detailTextLabel.text =  @"";
+//        
+//    } else {
+//        
+//        cell.accessoryType = UITableViewCellAccessoryNone;
+//        cell.detailTextLabel.text = [NSString stringWithFormat:@"%ld KB", item.stat.size / 1000];
+//    }
     
     return cell;
 }
-*/
 
-/*
+
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return UITableViewCellEditingStyleDelete;
 }
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"%s", __FUNCTION__);
+    if (editingStyle != UITableViewCellEditingStyleDelete) {
+        return;
+    }
+    [_datasource removeObjectAtIndex:indexPath.row];
+    [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+    [tableView reloadRowsAtIndexPaths:[tableView indexPathsForVisibleRows] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end

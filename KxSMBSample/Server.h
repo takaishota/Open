2136c887f2702464d0de804dc0d1be8712d8f25e
Shota Takai <s-takai@Shota-no-MacBook-Pro.local>
2015-04-06 11:12:36 +0900

    Serverクラスを追加

---
+++ KxSMBSample/Server.h
@@ -0,0 +1,14 @@
+//
+//  Server.h
+//  SMBFileReader
+//
+//  Created by ShotaTakai on 2015/04/05.
+//  Copyright (c) 2015年 Konstantin Bukreev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Server : NSObject
+@property (nonatomic) NSString *ip;
+@property (nonatomic) NSString *networkType;
+@end
---
+++ KxSMBSample/Server.m
@@ -0,0 +1,13 @@
+//
+//  Server.m
+//  SMBFileReader
+//
+//  Created by ShotaTakai on 2015/04/05.
+//  Copyright (c) 2015年 Konstantin Bukreev. All rights reserved.
+//
+
+#import "Server.h"
+
+@implementation Server
+
+@end

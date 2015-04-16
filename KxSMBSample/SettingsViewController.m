c0af045361cd0ccea18eef77514ff58bf3cd8568
Shota Takai <s-takai@Shota-no-MacBook-Pro.local>
2015-04-16 12:04:32 +0900

    環境設定画面の表示内容を追加

--- KxSMBSample/SettingsViewController.m
+++ KxSMBSample/SettingsViewController.m
@@ -7,26 +7,25 @@
 
 #import "SettingsViewController.h"
 
-@interface SettingsViewController ()
-
+@interface SettingsViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic) NSArray *settingsItems;
+@property (nonatomic) UITableView *tableView;
 @end
 
 @implementation SettingsViewController
-
+#pragma mark - LifeCycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = INFOCUS_UNDERLINE_COLOR;
+    _settingsItems = @[@"前回のログインアカウントを使用する", @"アプリ終了時にローカルファイルを削除する",@"クラウドサーバの利用を許可する"];
+    self.view.backgroundColor = [UIColor whiteColor];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
-    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(10, 20, 60, 10)];
     
-    [btn setTitle:@"閉じる" forState:UIControlStateNormal];
-    [btn setTitleColor:self.view.tintColor forState:UIControlStateNormal];
-    [self.view addSubview:btn];
+    [self.view addSubview:[self generateTableView]];
+    [self.view addSubview:[self generateCloseButton]];
     
-    [btn addTarget:self action:@selector(dismissSettingsView) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,18 +33,86 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Private
+- (UITableViewCell*)setupCellContents:(UITableViewCell*)cell LabelText:(NSString*)text {
+    cell.textLabel.text = text;
+    cell.textLabel.font = [UIFont fontWithName:@"HiraKakuProN-W3" size:12];
+    UISwitch *sw        = [[UISwitch alloc] initWithFrame:CGRectZero];
+    sw.onTintColor      = TOP_BACKGROUND_COLOR;
+    [sw addTarget:self action:@selector(switchValueChanged:) forControlEvents:UIControlEventValueChanged];
+    cell.accessoryView  = sw;
+    return cell;
+}
+
+- (void)switchValueChanged:(id)sender {
+    NSLog(@"valueChanged");
+}
+
 - (void)dismissSettingsView {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+// TODO:昨日作った分の実装
+- (UITableView*)generateTableView {
+    UITableView *tableView    = [[UITableView alloc] initWithFrame:CGRectMake(0,
+                                                                           0,
+                                                                           self.view.frame.size.width,
+                                                                           self.view.frame.size.height)
+                                                          style:UITableViewStylePlain];
+    tableView.dataSource      = self;
+    tableView.delegate        = self;
+    tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    tableView.scrollEnabled   = NO;
+    tableView.allowsSelection = NO;
+    return tableView;
 }
-*/
 
+static const CGFloat headerHeight = 60;
+- (UIButton*)generateCloseButton {
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(6, 0, 60, headerHeight)];
+    [button setTitle:@"閉じる" forState:UIControlStateNormal];
+    [button setTitleColor:self.view.tintColor forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(dismissSettingsView) forControlEvents:UIControlEventTouchUpInside];
+    return button;
+}
+
+#pragma mark - Table View Data Source
+- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.settingsItems count];
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell           = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1
+                                      reuseIdentifier:cellIdentifier];
+    }
+    cell = [self setupCellContents:cell LabelText:self.settingsItems[indexPath.row]];
+    return cell;
+}
+
+#pragma mark - Table View Delegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return headerHeight;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UILabel *label        = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, headerHeight)];
+    label.text            = @"環境設定";
+    label.font = [UIFont fontWithName:@"HiraKakuProN-W6" size:16];
+    label.textAlignment   = UITextAlignmentCenter;
+    label.backgroundColor = INFOCUS_UNDERLINE_COLOR;
+    return label;
+}
 @end

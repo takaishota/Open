8ef13f971a576576be2d11a4a556432ac497ecb2
Shota Takai <s-takai@nri-net.com>
2015-04-23 14:39:03 +0900

    セルのフォーマット、アニメーションの追加

--- KxSMBSample/ServerListCell.m
+++ KxSMBSample/ServerListCell.m
@@ -6,14 +6,22 @@
 //
 
 #import "ServerListCell.h"
+#import "UIColor+CustomColors.h"
+#import <POP/POP.h>
 
 @implementation ServerListCell
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 #pragma mark - Lifecycle
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        self.textLabel.textColor = [UIColor customGrayColor];
+        self.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        self.textLabel.font = [UIFont fontWithName:@"Avenir-Light" size:20];
+    }
+    return self;
 }
 
 - (void)layoutSubviews
@@ -21,7 +29,30 @@
     [super layoutSubviews];
     self.imageView.contentMode = UIViewContentModeScaleAspectFit;
     CGFloat W = 30;
-    self.imageView.frame = CGRectMake(self.imageView.frame.origin.x, self.imageView.frame.size.height/2 - W/2, W, W);
+    CGFloat xOffset = 50;
+    self.imageView.frame = CGRectMake(xOffset, self.frame.size.height/2 - W/2, W, W);
+}
+
+#pragma mark - Custom Accessors
+- (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated
+{
+    [super setHighlighted:highlighted animated:animated];
+    if (self.highlighted) {
+        POPBasicAnimation *scaleAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPViewScaleXY];
+        scaleAnimation.duration = 0.1;
+        scaleAnimation.toValue = [NSValue valueWithCGPoint:CGPointMake(0.95, 0.95)];
+        [self.textLabel pop_addAnimation:scaleAnimation forKey:@"scaleAnimation"];
+    } else {
+        POPSpringAnimation *scaleAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPViewScaleXY];
+        scaleAnimation.toValue = [NSValue valueWithCGPoint:CGPointMake(1, 1)];
+        scaleAnimation.velocity = [NSValue valueWithCGPoint:CGPointMake(2, 2)];
+        scaleAnimation.springBounciness = 20.f;
+        [self.textLabel pop_addAnimation:scaleAnimation forKey:@"scaleAnimation"];
+    }
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
 }
 
 @end

8a8defd5046cf7ff20fd247b9c600359462241b9
Shota Takai <s-takai@nri-net.com>
2015-05-18 10:08:31 +0900

    injectionプラグインの設定を削除

--- KxSMBSample/main.m
+++ KxSMBSample/main.m
@@ -17,12 +17,3 @@ int main(int argc, char *argv[])
     }
 }
 
-// From here to end of file added by Injection Plugin //
-
-#ifdef DEBUG
-static char _inMainFilePath[] = __FILE__;
-static const char *_inIPAddresses[] = {"127.0.0.1", "172.16.12.138", NULL};
-
-#define INJECTION_ENABLED
-#import "/Users/s-takai/Library/Application Support/Developer/Shared/Xcode/Plug-ins/InjectionPlugin.xcplugin/Contents/Resources/BundleInjection.h"
-#endif

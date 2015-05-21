619ec2706fba11055014b2cfdd9511bf7b5aa33d
Shota Takai <s-takai@nri-net.com>
2015-05-22 08:57:45 +0900

    OPNFileViewControllerクラスの状態を管理するクラスの作成

---
+++ KxSMBSample/OPNFileDataController.h
@@ -0,0 +1,13 @@
+//
+//  OPNFileDataController.h
+//  OPNFileReader
+//
+//  Created by Shota Takai on 2015/05/22.
+//  Copyright (c) 2015年 NRI Netcom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OPNFileDataController : NSObject
+
+@end
---
+++ KxSMBSample/OPNFileDataController.m
@@ -0,0 +1,13 @@
+//
+//  OPNFileDataController.m
+//  OPNFileReader
+//
+//  Created by Shota Takai on 2015/05/22.
+//  Copyright (c) 2015年 NRI Netcom. All rights reserved.
+//
+
+#import "OPNFileDataController.h"
+
+@implementation OPNFileDataController
+
+@end
